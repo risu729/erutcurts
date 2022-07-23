29c082f94466b677f74d2d970e7edf92be86d814
Risu <79110363+risu729@users.noreply.github.com>
2022-07-23 07:17:52 +0000

    Added debug messages

--- src/main/java/io/github/risu729/erutcurts/util/AttachmentUtil.java
+++ src/main/java/io/github/risu729/erutcurts/util/AttachmentUtil.java
@@ -145,7 +145,11 @@ public final class AttachmentUtil {
     Optional<UUID> uuid = getUUID(message);
     io.github.risu729.erutcurts.command.UtilCommands.replyDebugInfo(message, uuid); // DEBUG
     if (uuid.isEmpty()) {
+      io.github.risu729.erutcurts.command.UtilCommands.replyDebugInfo(message, "EMPTY!!!"); // DEBUG
       return List.of(message);
+    }
+    for (Message temp = message; temp != null; temp = temp.getReferencedMessage()) {
+      io.github.risu729.erutcurts.command.UtilCommands.replyDebugInfo(message, temp); // DEBUG
     }
     return Stream.iterate(message,
             m -> m != null /*&& m.getAuthor().getIdLong() == selfUserID && getUUID(m).equals(uuid)*/, Message::getReferencedMessage)
