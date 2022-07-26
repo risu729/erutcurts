b221929a048ac92d1e50e3db18e0fa52c6538ee8
Risu <79110363+risu729@users.noreply.github.com>
2022-07-26 20:11:42 +0900

    Update Main.java

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,6 +5,6 @@ import java.util.stream.*;
 
 public final class Main {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello World");
+        System.out.println("Hello World");
   }
 }
