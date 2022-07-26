18e0fe7935240819616d7430b915c91dfad4c246
Risu <79110363+risu729@users.noreply.github.com>
2022-07-26 20:11:24 +0900

    Update Main.java

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,4 +1,3 @@
-import io.github.risu729.erutcurts.util.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.function.*;
@@ -6,6 +5,6 @@ import java.util.stream.*;
 
 public final class Main {
   public static void main(String[] args) throws Exception {
-    System.out.println(FileUtil.isExtension(Path.of("src.txt"), Set.of("txt")));
+    System.out.println("Hello World");
   }
 }
